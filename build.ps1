b85910159a3a2de3395df97a4391b2729cd2967b
Adam Schroder <adamschroder@gmail.com>
2012-05-14 01:20:15 +1000

    Add 35 build to build script and remove unused reference

--- build.ps1
+++ build.ps1
@@ -12,11 +12,17 @@ Properties {
 
 FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
 
-Task Default -Depends Build
+Task Default -Depends Build35
 
-Task Build -Depends Clean { 
-    Write-Host "Building $solution_file" -ForegroundColor Green
-    Exec { msbuild "$solution_file" /t:Build /p:Configuration=Release /v:quiet /p:OutDir=$build_artifacts_dir } 
+Task Build35 -Depends Build40 { 
+	Write-Host "Building 3.5 $solution_file" -ForegroundColor Green
+	Exec { msbuild "$solution_file" /t:Clean /p:Configuration=Release } 
+	Exec { c:\windows\microsoft.net\framework\v3.5\msbuild.exe "$solution_file" /t:Build /p:Configuration=Release /v:quiet /p:DefineConstants="POCO_NO_DYNAMIC" /p:OutDir="$build_artifacts_dir\35\" }
+}
+
+Task Build40 -Depends Clean { 
+    Write-Host "Building 4.0 $solution_file" -ForegroundColor Green
+	Exec { msbuild "$solution_file" /t:Build /p:Configuration=Release /v:quiet /p:OutDir="$build_artifacts_dir\40\" } 
 }
 
 Task Clean {
