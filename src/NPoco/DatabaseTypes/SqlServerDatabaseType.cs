ebc28380b18764b64eadc5357c331a1441f0597d
Adam Schroder <adamschroder@gmail.com>
2012-11-30 14:49:24 +1100

    Fix Sql Server paging when using alias' and back to using scope_identity()

--- src/NPoco/DatabaseTypes/SqlServerDatabaseType.cs
+++ src/NPoco/DatabaseTypes/SqlServerDatabaseType.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
@@ -8,7 +9,7 @@ namespace NPoco.DatabaseTypes
     {
         public override string BuildPageQuery(long skip, long take, PagingHelper.SQLParts parts, ref object[] args)
         {
-            parts.sqlSelectRemoved = "peta_inner.* FROM (SELECT " + PagingHelper.rxOrderBy.Replace(parts.sqlSelectRemoved, "", 1) + ") peta_inner";
+            parts.sqlSelectRemoved = PagingHelper.rxOrderBy.Replace(parts.sqlSelectRemoved, "", 1);
             var sqlPage = string.Format("SELECT * FROM (SELECT ROW_NUMBER() OVER ({0}) peta_rn, {1}) peta_paged WHERE peta_rn>@{2} AND peta_rn<=@{3}",
                                         parts.sqlOrderBy ?? "ORDER BY (SELECT NULL /*poco_dual*/)", parts.sqlSelectRemoved, args.Length, args.Length + 1);
             args = args.Concat(new object[] { skip, skip + take }).ToArray();
@@ -18,18 +19,13 @@ namespace NPoco.DatabaseTypes
 
         public override object ExecuteInsert(Database db, IDbCommand cmd, string primaryKeyName)
         {
-            cmd.CommandText = "DECLARE @idt table(id bigint);" + cmd.CommandText + ";SELECT id FROM @idt";
+            cmd.CommandText = cmd.CommandText + ";SELECT SCOPE_IDENTITY();";
             return db.ExecuteScalarHelper(cmd);
         }
 
         public override string GetExistsSql()
         {
             return "IF EXISTS (SELECT 1 FROM {0} WHERE {1}) SELECT 1 ELSE SELECT 0";
-        }
-
-        public override string GetInsertOutputClause(string primaryKeyName)
-        {
-            return String.Format(" OUTPUT INSERTED.[{0}] into @idt", primaryKeyName);
         }
     }
 }
