b33beda715d648f8dce18947c04bc52f72a6e145
Adam Schroder <adamschroder@gmail.com>
2013-05-05 15:19:25 +1000

    Revert generic paging code

--- src/NPoco/PagerHelper.cs
+++ src/NPoco/PagerHelper.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Data;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace NPoco
 {
@@ -8,6 +6,7 @@ namespace NPoco
     {
         public static Regex rxColumns = new Regex(@"\A\s*SELECT\s+((?:\((?>\((?<depth>)|\)(?<-depth>)|.?)*(?(depth)(?!))\)|.)*?)(?<!,\s+)\bFROM\b", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.Compiled);
         public static Regex rxOrderBy = new Regex(@"\bORDER\s+BY\s+(?:\((?>\((?<depth>)|\)(?<-depth>)|.?)*(?(depth)(?!))\)|[\w\(\)\.\[\]""`])+(?:\s+(?:ASC|DESC))?(?:\s*,\s*(?:\((?>\((?<depth>)|\)(?<-depth>)|.?)*(?(depth)(?!))\)|[\w\(\)\.\[\]""`])+(?:\s+(?:ASC|DESC))?)*", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.Compiled);
+        public static Regex rxDistinct = new Regex(@"\ADISTINCT\s", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.Compiled);
 
         public struct SQLParts
         {
@@ -15,7 +14,6 @@ namespace NPoco
             public string sqlCount;
             public string sqlSelectRemoved;
             public string sqlOrderBy;
-            public string sqlUnordered;
         }
 
         public static bool SplitSQL(string sql, out SQLParts parts)
@@ -24,26 +22,33 @@ namespace NPoco
             parts.sqlSelectRemoved = null;
             parts.sqlCount = null;
             parts.sqlOrderBy = null;
-            parts.sqlUnordered = sql.Trim().Trim(';');
 
             // Extract the columns from "SELECT <whatever> FROM"
             var m = rxColumns.Match(sql);
-            if (!m.Success) return false;
+            if (!m.Success)
+                return false;
 
-            // Save column list  [and replace with COUNT(*)]
+            // Save column list and replace with COUNT(*)
             Group g = m.Groups[1];
             parts.sqlSelectRemoved = sql.Substring(g.Index);
 
+            if (rxDistinct.IsMatch(parts.sqlSelectRemoved))
+            {
+                parts.sqlCount = sql.Substring(0, g.Index) + "COUNT(" + m.Groups[1].ToString().Trim() + ") " + sql.Substring(g.Index + g.Length);
+            }
+            else
+            {
+                parts.sqlCount = sql.Substring(0, g.Index) + "COUNT(*) " + sql.Substring(g.Index + g.Length);
+            }
+
             // Look for the last "ORDER BY <whatever>" clause not part of a ROW_NUMBER expression
-            m = rxOrderBy.Match(parts.sql);
+            m = rxOrderBy.Match(parts.sqlCount);
             if (m.Success)
             {
                 g = m.Groups[0];
                 parts.sqlOrderBy = g.ToString();
-                parts.sqlUnordered = parts.sqlUnordered.Replace(parts.sqlOrderBy, "");
+                parts.sqlCount = parts.sqlCount.Substring(0, g.Index) + parts.sqlCount.Substring(g.Index + g.Length);
             }
-
-            parts.sqlCount = string.Format(@"SELECT COUNT(*) FROM ({0}) peta_tbl", parts.sqlUnordered);
 
             return true;
         }
