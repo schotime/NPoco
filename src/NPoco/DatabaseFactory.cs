de12351a6bc579e06d9631be01cbb4380056be34
Thanigainathan <thanigainathan@hotmail.com>
2013-06-21 15:35:09 +0530

    Update DatabaseFactory.cs
    Added code comments which help building help document

--- src/NPoco/DatabaseFactory.cs
+++ src/NPoco/DatabaseFactory.cs
@@ -8,16 +8,24 @@ using NPoco.FluentMappings;
 
 namespace NPoco
 {
+    /// <Summary>
+    /// A factory class to build database using mapping or fluency
+    /// </Summary>
     public class DatabaseFactory
     {
         private DatabaseFactoryConfigOptions _options;
 
+        /// <Summary>
+        /// Get the configuration setting
+        /// </Summary>
         public DatabaseFactoryConfig Config()
         {
             _options = new DatabaseFactoryConfigOptions();
             return new DatabaseFactoryConfig(_options);
         }
-
+        /// <Summary>
+        /// Build the database item with either mapper or pocodatafactory
+        /// </Summary>
         public Database Build(Database database)
         {
             if (_options.Mapper != null)
@@ -28,7 +36,10 @@ namespace NPoco
 
             return database;
         }
-
+    
+        /// <Summary>
+        /// Gets the database after building with options
+        /// </Summary>
         public Database GetDatabase()
         {
             if (_options.Database == null)
@@ -50,24 +61,32 @@ namespace NPoco
     public class DatabaseFactoryConfig
     {
         private readonly DatabaseFactoryConfigOptions _options;
-        
+        /// <Summary>
+        /// Constructor 
+        /// </Summary>
         public DatabaseFactoryConfig(DatabaseFactoryConfigOptions options)
         {
             _options = options;
         }
-        
+        /// <Summary>
+        /// Configure database usign function lambda argument 
+        /// </Summary>
         public DatabaseFactoryConfig UsingDatabase(Func<Database> database)
         {
             _options.Database = database;
             return this;
         }
-
+        /// <Summary>
+        /// Database mapping using mapper technique
+        /// </Summary>
         public DatabaseFactoryConfig WithMapper(IMapper mapper)
         {
             _options.Mapper = mapper;
             return this;
         }
-
+        /// <Summary>
+        /// Database mapping using fluent technique
+        /// </Summary>
         public DatabaseFactoryConfig WithFluentConfig(FluentConfig pocoDataFactory)
         {
             _options.PocoDataFactory = pocoDataFactory.Config(_options.Mapper);
