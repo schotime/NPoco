bf510357d77e1faa745b754ee1c4d8503a8858a2
David Roberts <david.roberts63@gmail.com>
2013-09-08 15:34:14 -0500

    Add multi-poco paging support

---
+++ src/NPoco.Tests/Common/CustomerUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NPoco.Tests.Common
+{
+    public class CustomerUser
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+    }
+}
