07baf44b6d5928f87b9918f22d03b324d578bbe7
Ivan Masmitjà <imasmitja@gmail.com>
2014-02-05 15:07:16 +0100

    Add "returning" primary key

--- src/NPoco/DatabaseTypes/FirebirdDatabaseType.cs
+++ src/NPoco/DatabaseTypes/FirebirdDatabaseType.cs
@@ -1,4 +1,7 @@
 
+using System;
+using System.Data;
+
 namespace NPoco.DatabaseTypes
 {
     public class FirebirdDatabaseType : DatabaseType
@@ -22,6 +25,25 @@ namespace NPoco.DatabaseTypes
         public override string GetDefaultInsertSql(string tableName, string[] names, string[] parameters)
         {
             return string.Format("INSERT INTO {0} ({1}) VALUES ({2})", EscapeTableName(tableName), string.Join(",", names), string.Join(",", parameters));
+        }
+
+        public override object ExecuteInsert<T>(Database db, IDbCommand cmd, string primaryKeyName, T poco, object[] args)
+        {
+            if (primaryKeyName != null)
+            {
+                cmd.CommandText += string.Format(" returning {0}", EscapeSqlIdentifier(primaryKeyName));
+                var param = cmd.CreateParameter();
+                param.ParameterName = primaryKeyName;
+                param.Value = DBNull.Value;
+                param.Direction = ParameterDirection.ReturnValue;
+                param.DbType = DbType.Int64;
+                cmd.Parameters.Add(param);
+                db.ExecuteNonQueryHelper(cmd);
+                return param.Value;
+            }
+
+            db.ExecuteNonQueryHelper(cmd);
+            return -1;
         }
 
         public override string GetProviderName()
