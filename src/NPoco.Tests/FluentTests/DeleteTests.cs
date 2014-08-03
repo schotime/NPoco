8f85ef93bac5a274d0b957a998e5dd7599f1a0b6
Adam Schroder <adamschroder@gmail.com>
2014-08-03 10:14:27 +1000

    Add delete tests

---
+++ src/NPoco.Tests/FluentTests/DeleteTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NPoco.Tests.Common;
+using NUnit.Framework;
+
+namespace NPoco.Tests.FluentTests
+{
+    [TestFixture]
+    public class DeleteTests : BaseDBFuentTest
+    {
+        [Test]
+        public void TestDelete()
+        {
+            Database.Delete<User>(1);
+            var user1 = Database.SingleOrDefaultById<User>(1);
+            Assert.IsNull(user1);
+        }
+    }
+}
