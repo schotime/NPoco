f220c194cb4720e0c202dda6f7c7244f8042d36a
Anders Jonsson <anders.jonsson@gmail.com>
2015-03-20 09:52:22 +0100

    Added support for generic dictionary as parameter

--- src/NPoco/ParameterHelper.cs
+++ src/NPoco/ParameterHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -33,8 +34,27 @@ namespace NPoco
                     // Look for a property on one of the arguments with this name
                     bool found = false;
                     arg_val = null;
+
                     foreach (var o in args_src)
                     {
+                        var dictionary = o as IDictionary;
+                        if (dictionary != null)
+                        {
+                            var idic = dictionary;
+                            Type[] arguments = idic.GetType().GetGenericArguments();
+
+                            if (arguments[0] == typeof(string))
+                            {
+                                var val = idic[param];
+                                if (val != null)
+                                {
+                                    found = true;
+                                    arg_val = val;
+                                    break;
+                                }
+                            }
+                        }
+
                         var pi = o.GetType().GetProperty(param);
                         if (pi != null)
                         {
@@ -47,6 +67,8 @@ namespace NPoco
                     if (!found)
                         throw new ArgumentException(String.Format("Parameter '@{0}' specified but none of the passed arguments have a property with this name (in '{1}')", param, _sql));
                 }
+
+
 
                 // Expand collections to parameter lists
                 if ((arg_val as System.Collections.IEnumerable) != null &&
