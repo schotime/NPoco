dd1708ea54ac5c05fdedfa45eff6b6be3135d7f6
Anders Jonsson <anders.jonsson@gmail.com>
2015-03-20 10:50:06 +0100

    Support for generic dictionary as parameter

---
+++ src/NPoco.Tests/ParameterHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NPoco.Tests
+{
+    public class ParameterHelperTests
+    {
+        [Test]
+        public void TestGenericDictionaryAsParameter()
+        {
+            var sql = "SELECT * FROM test WHERE testID = @testID AND testName = @testName";
+            const string testName = "testName";
+            const int testID = 1;
+            
+            var dict = new Dictionary<string, object>();
+            dict["testName"] = testName;
+            dict["testID"] = testID;
+            
+            var args = new List<object>();
+            var resultSql = ParameterHelper.ProcessParams(sql, new[] {dict}, args);
+
+            Assert.AreEqual(testID, args[0]);
+            Assert.AreEqual(testName, args[1]);
+            Assert.AreEqual("SELECT * FROM test WHERE testID = @0 AND testName = @1", resultSql);
+        }
+    }
+}
