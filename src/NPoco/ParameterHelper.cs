d9b23117cf2c31eafc1249e1a46559839bc43e71
Anders Jonsson <anders.jonsson@gmail.com>
2015-03-20 09:53:36 +0100

    -

--- src/NPoco/ParameterHelper.cs
+++ src/NPoco/ParameterHelper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
