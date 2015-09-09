94a74e08e2f7c0fc3c9dda898fde906f93e6e67f
Adam Schroder <adamschroder@gmail.com>
2015-09-09 20:40:26 +1000

    Remove unused property

--- src/NPoco/RowMappers/RowMapperContext.cs
+++ src/NPoco/RowMappers/RowMapperContext.cs
@@ -5,7 +5,6 @@ namespace NPoco.RowMappers
     public struct RowMapperContext
     {
         public object Instance { get; set; }
-        public object PrimaryKeyValue { get; set; }
         public PocoData PocoData { get; set; }
         public Type Type { get { return PocoData.Type; } }
     }
