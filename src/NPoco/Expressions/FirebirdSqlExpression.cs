e87a3dfa94758f8e55ebd4f364de96d4c011615a
Ivan Masmitja <imasmitja@gmail.com>
2015-09-30 12:21:56 +0200

    Firebird doesn't support boolean type

--- src/NPoco/Expressions/FirebirdSqlExpression.cs
+++ src/NPoco/Expressions/FirebirdSqlExpression.cs
@@ -18,5 +18,25 @@ namespace NPoco.Expressions
             else
                 return string.Format("substring({0} FROM {1})", columnName, startIndex); 
         }
+
+        protected override object GetTrueExpression()
+        {
+            return new PartialSqlString("(1=1)");
+        }
+
+        protected override object GetFalseExpression()
+        {
+            return new PartialSqlString("(0=1)");
+        }
+
+        protected override object GetQuotedTrueValue()
+        {
+            return CreateParam(1);
+        }
+
+        protected override object GetQuotedFalseValue()
+        {
+            return CreateParam(0);
+        }
     }
 }
