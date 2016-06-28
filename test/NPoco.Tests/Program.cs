13099422a854ceb839132f05d7ce005e84e51efd
Adam Schroder <adamschroder@gmail.com>
2016-06-28 16:49:12 +1000

    Upgrade to .net core 1.0

--- test/NPoco.Tests/Program.cs
+++ test/NPoco.Tests/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using NUnitLite;
+using NUnit.Common;
+using NUnit.Framework;
 using System.Reflection;
 
 namespace NPoco.Tests
@@ -14,7 +16,8 @@ namespace NPoco.Tests
 #if NET452
             return new AutoRun().Execute(args);
 #else
-            return new AutoRun().Execute(typeof(Program).GetTypeInfo().Assembly, Console.Out, Console.In, args);
+            return new AutoRun(typeof(Program).GetTypeInfo().Assembly)
+                .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
 #endif
         }
     }
