f67f6f17832aabe1ba594d9062778f79f77b9eea
Adam Schroder <adamschroder@gmail.com>
2017-06-09 09:46:51 +1000

    Fix mapping to structs

--- src/NPoco/MemberAccessor.cs
+++ src/NPoco/MemberAccessor.cs
@@ -162,46 +162,44 @@ namespace NPoco
 
         private Func<object, object> GetGetDelegate()
         {
-            Type[] setParamTypes = new[] { typeof(object) };
-            Type setReturnType = typeof (object);
+            Type setParamType = typeof(object);
+            Type[] setParamTypes = { setParamType };
+            Type setReturnType = typeof(object);
 
-            var owner = _targetType.GetTypeInfo().IsAbstract || _targetType.GetTypeInfo().IsInterface ? null : _targetType;
-            var getMethod = owner != null 
+            Type owner = _targetType.GetTypeInfo().IsAbstract || _targetType.GetTypeInfo().IsInterface ? null : _targetType;
+            var getMethod = owner != null
                 ? new DynamicMethod(Guid.NewGuid().ToString(), setReturnType, setParamTypes, owner, true)
                 : new DynamicMethod(Guid.NewGuid().ToString(), setReturnType, setParamTypes, true);
 
             // From the method, get an ILGenerator. This is used to
             // emit the IL that we want.
             ILGenerator getIL = getMethod.GetILGenerator();
-            
-            getIL.DeclareLocal(typeof(object));
-            getIL.Emit(OpCodes.Ldarg_0); //Load the first argument
-            //(target object)
-            //Cast to the source type
-            getIL.Emit(OpCodes.Castclass, this._targetType);
-            //Get the property value
 
+            getIL.Emit(OpCodes.Ldarg_0); //Load the first argument (target object)
+            getIL.Emit(_targetType.GetTypeInfo().IsValueType ? OpCodes.Unbox : OpCodes.Castclass, _targetType);
+
+            Type returnType;
             if (_member.IsField())
             {
                 getIL.Emit(OpCodes.Ldfld, (FieldInfo)_member);
-                if (_memberType.GetTypeInfo().IsValueType)
-                {
-                    getIL.Emit(OpCodes.Box, _memberType);
-                }
+                returnType = _memberType;
             }
             else
             {
-                var targetGetMethod = ((PropertyInfo) _member).GetGetMethod();
-                getIL.Emit(OpCodes.Callvirt, targetGetMethod);
-                if (targetGetMethod.ReturnType.GetTypeInfo().IsValueType)
-                {
-                    getIL.Emit(OpCodes.Box, targetGetMethod.ReturnType);
-                }
+                var targetGetMethod = ((PropertyInfo)_member).GetGetMethod();
+                var opCode = _targetType.GetTypeInfo().IsValueType ? OpCodes.Call : OpCodes.Callvirt;
+                getIL.Emit(opCode, targetGetMethod);
+                returnType = targetGetMethod.ReturnType;
+            }
+
+            if (returnType.GetTypeInfo().IsValueType)
+            {
+                getIL.Emit(OpCodes.Box, returnType);
             }
 
             getIL.Emit(OpCodes.Ret);
 
-            var del = getMethod.CreateDelegate(Expression.GetFuncType(setParamTypes.Concat(new[]{setReturnType}).ToArray()));
+            var del = getMethod.CreateDelegate(Expression.GetFuncType(setParamType, setReturnType));
             return del as Func<object, object>;
         }
     }
