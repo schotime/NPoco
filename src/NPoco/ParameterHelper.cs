7af5bbbd248095692529179f6f397b07c446ad14
Adam Schroder <adamschroder@gmail.com>
2017-08-16 12:56:20 +1000

    Fix parameter reuse to include IN statements. fixes #405

--- src/NPoco/ParameterHelper.cs
+++ src/NPoco/ParameterHelper.cs
@@ -13,106 +13,109 @@ namespace NPoco
         // Helper to handle named parameters from object properties
         public static Regex rxParamsPrefix = new Regex(@"(?<!@)@\w+", RegexOptions.Compiled);
 
-        public static string ProcessParams(string _sql, object[] args_src, List<object> args_dest)
+        public static string ProcessParams(string sql, object[] args_src, List<object> args_dest)
         {
-            var parameters = new Dictionary<string, int>();
-            return rxParamsPrefix.Replace(_sql, m =>
+            var parameters = new Dictionary<string, string>();
+            return rxParamsPrefix.Replace(sql, m =>
             {
-                string param = m.Value.Substring(1);
+                string item;
+                if (parameters.TryGetValue(m.Value, out item))
+                    return item;
 
-                if (parameters.ContainsKey(param))
+                item = parameters[m.Value] = ProcessParam(ref sql, m.Value, args_src, args_dest);
+                return item;
+            });
+        }
+        
+        public static string ProcessParam(ref string sql, string rawParam, object[] args_src, List<object> args_dest)
+        {
+            string param = rawParam.Substring(1);
+
+            object arg_val;
+
+            int paramIndex;
+            if (Int32.TryParse(param, out paramIndex))
+            {
+                // Numbered parameter
+                if (paramIndex < 0 || paramIndex >= args_src.Length)
+                    throw new ArgumentOutOfRangeException(String.Format("Parameter '@{0}' specified but only {1} parameters supplied (in `{2}`)", paramIndex, args_src.Length, sql));
+                arg_val = args_src[paramIndex];
+            }
+            else
+            {
+                // Look for a property on one of the arguments with this name
+                bool found = false;
+                arg_val = null;
+                foreach (var o in args_src)
                 {
-                    return "@" + parameters[param];
-                }
-
-                parameters[param] = args_dest.Count;
-
-                object arg_val;
-
-                int paramIndex;
-                if (Int32.TryParse(param, out paramIndex))
-                {
-                    // Numbered parameter
-                    if (paramIndex < 0 || paramIndex >= args_src.Length)
-                        throw new ArgumentOutOfRangeException(String.Format("Parameter '@{0}' specified but only {1} parameters supplied (in `{2}`)", paramIndex, args_src.Length, _sql));
-                    arg_val = args_src[paramIndex];
-                }
-                else
-                {
-                    // Look for a property on one of the arguments with this name
-                    bool found = false;
-                    arg_val = null;
-                    foreach (var o in args_src)
+                    var dict = o as IDictionary;
+                    if (dict != null)
                     {
-                        var dict = o as IDictionary;
-                        if (dict != null)
-                        {
-                            Type[] arguments = dict.GetType().GetGenericArguments();
+                        Type[] arguments = dict.GetType().GetGenericArguments();
 
-                            if (arguments[0] == typeof(string))
+                        if (arguments[0] == typeof(string))
+                        {
+                            var val = dict[param];
+                            if (val != null)
                             {
-                                var val = dict[param];
-                                if (val != null)
-                                {
-                                    found = true;
-                                    arg_val = val;
-                                    break;
-                                }
+                                found = true;
+                                arg_val = val;
+                                break;
                             }
                         }
-
-                        var pi = o.GetType().GetProperty(param);
-                        if (pi != null)
-                        {
-                            arg_val = pi.GetValue(o, null);
-                            found = true;
-                            break;
-                        }
                     }
 
-                    if (!found)
-                        throw new ArgumentException(String.Format("Parameter '@{0}' specified but none of the passed arguments have a property with this name (in '{1}')", param, _sql));
-                }
-
-                // Expand collections to parameter lists
-                if ((arg_val as System.Collections.IEnumerable) != null &&
-                    (arg_val as string) == null &&
-                    (arg_val as byte[]) == null)
-                {
-                    var sb = new StringBuilder();
-                    foreach (var i in arg_val as System.Collections.IEnumerable)
+                    var pi = o.GetType().GetProperty(param);
+                    if (pi != null)
                     {
-                        var indexOfExistingValue = args_dest.IndexOf(i);
-                        if (indexOfExistingValue >= 0)
-                        {
-                            sb.Append((sb.Length == 0 ? "@" : ",@") + indexOfExistingValue);
-                        }
-                        else
-                        {
-                            sb.Append((sb.Length == 0 ? "@" : ",@") + args_dest.Count);
-                            args_dest.Add(i);
-                        }
+                        arg_val = pi.GetValue(o, null);
+                        found = true;
+                        break;
                     }
-                    if (sb.Length == 0)
-                    {
-                        Type type = typeof(string);
-                        var t = arg_val.GetType();
-                        if (t.IsArray)
-                            type = t.GetElementType();
-                        else if (t.IsOrHasGenericInterfaceTypeOf(typeof(IEnumerable<>)))
-                            type = t.GetGenericArguments().First();
+                }
 
-                        sb.AppendFormat($"select @{args_dest.Count} /*poco_dual*/ where 1 = 0");
-                        args_dest.Add(MappingHelper.GetDefault(type));
-                    }
-                    return sb.ToString();
-                }
-                else
+                if (!found)
+                    throw new ArgumentException(String.Format("Parameter '@{0}' specified but none of the passed arguments have a property with this name (in '{1}')", param, sql));
+            }
+
+            // Expand collections to parameter lists
+            if ((arg_val as System.Collections.IEnumerable) != null &&
+                (arg_val as string) == null &&
+                (arg_val as byte[]) == null)
+            {
+                var sb = new StringBuilder();
+                foreach (var i in arg_val as System.Collections.IEnumerable)
                 {
-                    args_dest.Add(arg_val);
-                    return "@" + (args_dest.Count - 1).ToString();
+                    var indexOfExistingValue = args_dest.IndexOf(i);
+                    if (indexOfExistingValue >= 0)
+                    {
+                        sb.Append((sb.Length == 0 ? "@" : ",@") + indexOfExistingValue);
+                    }
+                    else
+                    {
+                        sb.Append((sb.Length == 0 ? "@" : ",@") + args_dest.Count);
+                        args_dest.Add(i);
+                    }
                 }
-            });
+                if (sb.Length == 0)
+                {
+                    Type type = typeof(string);
+                    var t = arg_val.GetType();
+                    if (t.IsArray)
+                        type = t.GetElementType();
+                    else if (t.IsOrHasGenericInterfaceTypeOf(typeof(IEnumerable<>)))
+                        type = t.GetGenericArguments().First();
+
+                    sb.AppendFormat($"select @{args_dest.Count} /*poco_dual*/ where 1 = 0");
+                    args_dest.Add(MappingHelper.GetDefault(type));
+                }
+                return sb.ToString();
+            }
+            else
+            {
+                args_dest.Add(arg_val);
+                return "@" + (args_dest.Count - 1).ToString();
+            }
         }
     }
 }
--- test/NPoco.Tests/ParameterHelper.cs
+++ test/NPoco.Tests/ParameterHelper.cs
@@ -77,5 +77,34 @@ namespace NPoco.Tests
             Assert.AreEqual(99, args[0]);
             Assert.AreEqual(89, args[1]);
         }
+
+        [Test]
+        public void TestMultipleInClauses()
+        {
+            var sql = "SELECT * FROM test WHERE testID in (@0) and test2ID in (@0)";
+
+            var list = new List<int>() { 1, 2, 3 };
+            var args = new List<object>();
+            var resultSql = ParameterHelper.ProcessParams(sql, new[] { list }, args);
+
+            var expectedSql = "SELECT * FROM test WHERE testID in (@0,@1,@2) and test2ID in (@0,@1,@2)";
+
+            Assert.AreEqual(expectedSql, resultSql);
+        }
+
+        [Test]
+        public void TestMultipleInClausesPerf()
+        {
+            var sql = "SELECT * FROM test WHERE testID in (@0) and test2ID in (@0) and asdf = @1 and asdf = @2 and asdf = @3 and asdf = @4 and asdf = @5";
+
+            var list = new List<int>() { 1, 2, 3, 6, 5, 4 };
+            var args = new List<object>();
+
+            var expectedSql = "SELECT * FROM test WHERE testID in (@0,@1,@2,@3,@4,@5) and test2ID in (@0,@1,@2,@3,@4,@5) and asdf = @6 and asdf = @7 and asdf = @8 and asdf = @9 and asdf = @10";
+
+            var resultSql = ParameterHelper.ProcessParams(sql, new object[] { list, 1,2,3,4,5 }, args);
+
+            Assert.AreEqual(expectedSql, resultSql);
+        }
     }
 }
